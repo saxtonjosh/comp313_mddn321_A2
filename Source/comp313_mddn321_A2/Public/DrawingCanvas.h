c76921b98c475e609005dd25fd249c84ebc43654
Pham Dong <dongpham@ecs.vuw.ac.nz>
2021-09-06 14:04:50 +1200

    feat :Black trails and background to draw on finished

--- Source/comp313_mddn321_A2/Public/DrawingCanvas.h
+++ Source/comp313_mddn321_A2/Public/DrawingCanvas.h
@@ -3,26 +3,43 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine/Texture2D.h"
+#include "Object.h"
 #include "Components/SceneComponent.h"
 #include "DrawingCanvas.generated.h"
 
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
-class COMP313_MDDN321_A2_API UDrawingCanvas : public USceneComponent
+UCLASS(Blueprintable, BlueprintType)
+class COMP313_MDDN321_A2_API UDrawingCanvas : public UObject
 {
-	GENERATED_BODY()
-
-public:	
-	// Sets default values for this component's properties
-	UDrawingCanvas();
-
-protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
-		
+    GENERATED_BODY()
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Variables)
+        UTexture2D* dynamicCanvas;
+    UFUNCTION(BlueprintCallable, Category = DrawingTools)
+        void InitializeCanvas(const int32 pixelsH, const int32 pixelsV);
+    UFUNCTION(BlueprintCallable, Category = DrawingTools)
+        void UpdateCanvas();
+    UFUNCTION(BlueprintCallable, Category = DrawingTools)
+        void ClearCanvas();
+	UFUNCTION(BlueprintCallable, Category = DrawingTools)
+        void InitializeDrawingTools(const int32 brushRadius);
+    UFUNCTION(BlueprintCallable, Category = DrawingTools)
+        void DrawDot(const int32 pixelCoordX, const int32 pixelCoordY);
+    UDrawingCanvas();
+    ~UDrawingCanvas();
+private:
+	// draw brush tool
+	std::unique_ptr<uint8[]> canvasBrushMask;
+    int radius;
+    int brushBufferSize;
+    // canvas
+    std::unique_ptr<uint8[]> canvasPixelData;
+    int canvasWidth;
+    int canvasHeight;
+    int bytesPerPixel;
+    int bufferPitch;
+    int bufferSize;
+    std::unique_ptr<FUpdateTextureRegion2D> echoUpdateTextureRegion;
+    void setPixelColor(uint8*& pointer, uint8 red, uint8 green, uint8 blue, uint8 alpha);
 };
